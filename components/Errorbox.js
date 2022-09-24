c68ed47c4c812e7db6e546596e4d56bcc008f2d7
Superdeveloper-rgb <62853708+Superdeveloper-rgb@users.noreply.github.com>
2022-09-24 02:28:42 -0400

    better errors and css

--- components/Errorbox.js
+++ components/Errorbox.js
@@ -1,11 +1,12 @@
 import styles from "../styles/errorbox.module.css"
 
-export default function Errorbox({title, message, code}) {
+export default function Errorbox({title, message, code, options, action}) {
     return(
         <div className={styles.wrapper}>
             <h1 className={styles.title}>{code && `${code} - `}{title}</h1>
             <p className={styles.message}>{message}</p>
-            {code != 404 && <a href="/" className={styles.reload}><i className="fas fa-redo"/>&nbsp; Try again </a>}
+            {options?.includes("reload") && <a href="/" className={styles.reload}><i className="fas fa-redo"/>&nbsp; Try again </a>}
+            {options?.includes("close") && <button className={styles.action} onClick={()=>action("close")}><i className="fas fa-times"/> Close</button>}
         </div>
     )
 }
