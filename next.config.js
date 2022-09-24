f5dfa5fba6193703bfc3f70569f49acfd0f543d0
Superdeveloper-rgb <62853708+Superdeveloper-rgb@users.noreply.github.com>
2022-09-23 23:40:17 -0400

    removed private stuff from this file :)

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    bunny_library_id: 59441,
-    bunny_api_key: "88daaeda-9b54-4f6f-9cdce8a8564a-5212-48f8"
-  }
 }
 
 module.exports = nextConfig
